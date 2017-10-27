da7e72f94f5b6f406f622350f798b4970d84700c
James Hartley <jhartley58@gmail.com>
2017-10-27 15:12:31 +0100

    Fix JS indentation

--- js/creative.js
+++ js/creative.js
@@ -1,91 +1,91 @@
 (function($) {
-    "use strict"; // Start of use strict
+  "use strict"; // Start of use strict
 
-    // Smooth scrolling using jQuery easing
-    $('a[href*="#"]:not([href="#"]):not([class*="popup"])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: (target.offset().top - 48)
-                }, 1000, "easeInOutExpo");
-                return false;
-            }
-        }
-    });
+  // Smooth scrolling using jQuery easing
+  $('a[href*="#"]:not([href="#"]):not([class*="popup"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 48)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
 
-    // Activate scrollspy to add active class to navbar items on scroll
-    $('body').scrollspy({
-        target: '#mainNav',
-        offset: 48
-    });
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 48
+  });
 
-    // Closes responsive menu when a link is clicked
-    $('.navbar-collapse>ul>li>a').click(function() {
-        $('.navbar-collapse').collapse('hide');
-    });
+  // Closes responsive menu when a link is clicked
+  $('.navbar-collapse>ul>li>a').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
 
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(function() {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
-        } else {
-            $("#mainNav").removeClass("navbar-shrink");
-        }
-    });
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  });
 
-    // Scroll reveal calls
-    window.sr = ScrollReveal();
-    sr.reveal('.sr-icons', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
-    }, 200);
-    sr.reveal('.sr-button', {
-        duration: 1000,
-        delay: 200
-    });
-    sr.reveal('.sr-contact', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
-    }, 300);
+  // Scroll reveal calls
+  window.sr = ScrollReveal();
+  sr.reveal('.sr-icons', {
+    duration: 600,
+    scale: 0.3,
+    distance: '0px'
+  }, 200);
+  sr.reveal('.sr-button', {
+    duration: 1000,
+    delay: 200
+  });
+  sr.reveal('.sr-contact', {
+    duration: 600,
+    scale: 0.3,
+    distance: '0px'
+  }, 300);
 
-    /*// Magnific popup calls
-    $('.popup-gallery').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1]
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-        }
+  /*// Magnific popup calls
+  $('.popup-gallery').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+          enabled: true,
+          navigateByImgClick: true,
+          preload: [0, 1]
+      },
+      image: {
+          tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+      }
     });*/
 
     $('.popup-with-zoom-anim').magnificPopup({
-        type: 'inline',
+      type: 'inline',
 
-        fixedContentPos: false,
-        fixedBgPos: true,
+      fixedContentPos: false,
+      fixedBgPos: true,
 
-        overflowY: 'auto',
+      overflowY: 'auto',
 
-        closeBtnInside: true,
-        preloader: false,
-        
-        midClick: true,
-        removalDelay: 300,
-        mainClass: 'my-mfp-zoom-in'
+      closeBtnInside: true,
+      preloader: false,
+
+      midClick: true,
+      removalDelay: 300,
+      mainClass: 'my-mfp-zoom-in'
     });
 
     $(document).ready(function() {
-    $('.popup-with-form').magnificPopup({
+      $('.popup-with-form').magnificPopup({
         type: 'inline',
         preloader: false,
         focus: '#name',
@@ -93,16 +93,16 @@
         // When elemened is focused, some mobile browsers in some cases zoom in
         // It looks not nice, so we disable it:
         callbacks: {
-            beforeOpen: function() {
-                if($(window).width() < 700) {
-                    this.st.focus = false;
-                } else {
-                    this.st.focus = '#name';
-                }
+          beforeOpen: function() {
+            if($(window).width() < 700) {
+              this.st.focus = false;
+            } else {
+              this.st.focus = '#name';
             }
+          }
         }
+      });
     });
-});
 
 })(jQuery); // End of use strict
 
@@ -117,25 +117,25 @@ bg_header.src="img/header.jpg";
 
 var fadeIn = function(){
 
-if (!bg_header.complete){
+  if (!bg_header.complete){
     fade_header.style.opacity = "0";
 
     bg_header.onload = function(){
-        fade_header.style.backgroundImage = "url(img/header.jpg)";
-        fade_header.style.transition = "opacity 1s ease-out";
-        fade_header.style.opacity = "1"; }
-    
+      fade_header.style.backgroundImage = "url(img/header.jpg)";
+      fade_header.style.transition = "opacity 1s ease-out";
+      fade_header.style.opacity = "1"; }
+      
     /*bg_service.onload = function(){
         fade_service.style.backgroundImage = "url(img/header_graduation.jpg)";
         fade_service.style.transition = "opacity 1s ease-out";
         fade_service.style.opacity = "1"; }*/
+      }
     }
-}
     
 var imgTmr = window.setTimeout(fadeIn,100); //may want to tailor 100ms
 
 /*$(".parallax").on("load", function() {
     window.clearTimeout(imgTmr);
-console.log("loaded");});*/
+    console.log("loaded");});*/
 
 //or timer, if not .complete after 100ms, do fade load
